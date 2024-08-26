86a4afc5c0ba796604eb142373c52ea80cd5559c
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-08-26 12:44:39 +0300

    Project Setup Complete

---
+++ ai-prompt-hub/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-full flex flex-col items-center'>
+        <h1 className='head_text text-center'>Discover & Share</h1>
+        <br className='max-md:hidden'/>
+        <span className='orange_gradient text-center'>AI-Powered Prompts</span>
+    </section>
+  )
+}
+
+export default Home
